8b3edc27b8514f03f287091a912f4ff2d53d4c8a
Silvio Waschina <s.waschina@nutrinf.uni-kiel.de>
2021-12-14 15:02:46 +0100

    removing cplexAPI from suggests

--- R/Eutropia-package.R
+++ R/Eutropia-package.R
@@ -3,3 +3,18 @@
 #' @useDynLib Eutropia, .registration = TRUE
 ## usethis namespace: end
 NULL
+
+
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Welcome to Eutropia.")
+}
+
+.onLoad <- function(libname, pkgname) {
+  if("cplexAPI" %in% installed.packages()) {
+    sybil::SYBIL_SETTINGS("SOLVER","cplexAPI")
+    cat("Solver: cplexAPI\n")
+  } else {
+    sybil::SYBIL_SETTINGS("SOLVER","glpkAPI")
+    cat("Solver: glpkAPI\n")
+  }
+}
